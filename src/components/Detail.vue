e0b7b00fbcf84ca3e83d37dd967163413ff28777
Thomas <esomly57@gmail.com>
2020-06-24 18:52:48 +0200

    setup

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -163,7 +163,7 @@ export default {
             this.$route.params.token
         )
         .then(res => {
-          console.log("Données" + res.data);
+          console.log("Données" + res);
           this.center.lat = res.data.x;
           this.center.lng = res.data.y;
           this.positionCentre.lat = res.data.x;
