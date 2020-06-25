0053cc431fef4243533de0f40035d8dc08d94788
Thomas <esomly57@gmail.com>
2020-06-25 10:31:58 +0200

    setup

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,90 +1,6 @@
 <template>
   <div>
-    <div class="eventlist">
-      <button
-        type="button"
-        class="btn btn-primary"
-        v-clipboard:copy="message"
-        v-clipboard:success="onCopy"
-        v-clipboard:error="onError"
-      >Partager</button>
-
-      <l-map
-        style="height: 400px;  width:100%"
-        :zoom="zoom"
-        :center="center"
-        :markerZoomAnimation="true"
-        attribution="ok"
-      >
-        <l-tile-layer :url="url"></l-tile-layer>
-        <l-marker :lat-lng="positionCentre" :draggable="false" :icon="icon"></l-marker>
-      </l-map>
-      {{events.titre}}
-      <div id="data">
-        <h2>Météo de {{meteod.name}}</h2>
-        <span>Tendance : {{meteod.weather[0].description}}</span>
-        <hr />
-        <span>Temperature : {{meteod.main.temp}}</span>
-        <hr />
-        <span>Pression : {{meteod.main.pressure}}hPa</span>
-        <hr />
-        <span>Pression : {{meteod.main.humidity}}%</span>
-        <hr />
-      </div>
-      <ul id="example-1">
-        <li v-for="item in items" :key="item.nom">{{ item.nom }}</li>
-      </ul>
-      <ul id="example-1">
-        <li
-          v-for="comment in comments"
-          :key="comment.nom"
-        >{{ comment.nom }}{{ comment.commentaire }}</li>
-        <button
-          type="button"
-          class="btn btn-primary"
-          data-toggle="modal"
-          data-target="#myModal"
-          data-backdrop="true"
-        >Commenter</button>
-      </ul>
-
-      <div
-        class="modal fade"
-        id="myModal"
-        tabindex="-1"
-        role="dialog"
-        aria-labelledby="exampleModalCenterTitle"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog modal-dialog-centered" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              <input
-                type="text"
-                id="name"
-                v-model="name"
-                name="name"
-                required
-                minlength="4"
-                maxlength="8"
-                size="10"
-                placeholder="nom"
-              />
-              <textarea id="name" v-model="description" name="name" />
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-success" v-on:click="commenter">Poster</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <p>ok</p>
   </div>
 </template>
 
@@ -238,7 +154,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-* {
-  background-color: red;
-}
 </style>
