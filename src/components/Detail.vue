b12e03982e35bce870c636bd82acc2528a5c2689
Thomas <esomly57@gmail.com>
2020-06-25 11:12:58 +0200

    setup

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -21,52 +21,20 @@
     <ul id="example-1">
       <li v-for="item in items" :key="item.nom">{{ item.nom }}</li>
     </ul>
-    <ul id="example-1">
-      <li v-for="comment in comments" :key="comment.nom">{{ comment.nom }}{{ comment.commentaire }}</li>
-      <button
-        type="button"
-        class="btn btn-primary"
-        data-toggle="modal"
-        data-target="#myModal"
-        data-backdrop="true"
-      >Commenter</button>
-    </ul>
-
-    <div
-      class="modal fade"
-      id="myModal"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="exampleModalCenterTitle"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <input
-              type="text"
-              id="name"
-              v-model="name"
-              name="name"
-              required
-              minlength="4"
-              maxlength="8"
-              size="10"
-              placeholder="nom"
-            />
-            <textarea id="name" v-model="description" name="name" />
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-success" v-on:click="commenter">Poster</button>
-          </div>
-        </div>
-      </div>
+    <div>
+      <ul id="example-1">
+        <li
+          v-for="comment in comments"
+          :key="comment.nom"
+        >{{ comment.nom }}{{ comment.commentaire }}</li>
+        <button
+          type="button"
+          class="btn btn-primary"
+          data-toggle="modal"
+          data-target="#myModal"
+          data-backdrop="true"
+        >Commenter</button>
+      </ul>
     </div>
   </div>
 </template>
