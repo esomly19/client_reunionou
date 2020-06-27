2add65f44a01b8aaba2afcb026096ed3356e706d
Thomas <esomly57@gmail.com>
2020-06-27 12:20:47 +0200

    setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,34 @@
 <template>
   <div class="home">
-    <p>Home</p>
+    <div id="demo" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ul class="carousel-indicators">
+        <li data-target="#demo" data-slide-to="0" class="active"></li>
+        <li data-target="#demo" data-slide-to="1"></li>
+        <li data-target="#demo" data-slide-to="2"></li>
+      </ul>
+
+      <!-- The slideshow -->
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="../assets/img/reun.jpg" alt="Reunion" />
+        </div>
+        <div class="carousel-item">
+          <img src="../assets/img/carte.jpg" alt="Carte" />
+        </div>
+        <div class="carousel-item">
+          <img src="../assets/img/cal.jpg" alt="Calendrier" />
+        </div>
+      </div>
+
+      <!-- Left and right controls -->
+      <a class="carousel-control-prev" href="#demo" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </a>
+      <a class="carousel-control-next" href="#demo" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </a>
+    </div>
   </div>
 </template>
 
