68a7703ef8a5165c106f12bd58995f538af568ee
Thomas <esomly57@gmail.com>
2020-06-27 12:25:29 +0200

    setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,34 @@
 <template>
   <div class="home">
     <div id="demo" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
       <ul class="carousel-indicators">
         <li data-target="#demo" data-slide-to="0" class="active"></li>
         <li data-target="#demo" data-slide-to="1"></li>
         <li data-target="#demo" data-slide-to="2"></li>
       </ul>
-
-      <!-- The slideshow -->
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="../assets/img/reun.jpg" alt="Reunion" />
+          <img src="../assets/img/cal.jpg" alt="Los Angeles" width="1100" height="500" />
+          <div class="carousel-caption">
+            <h3>Los Angeles</h3>
+            <p>We had such a great time in LA!</p>
+          </div>
         </div>
         <div class="carousel-item">
-          <img src="../assets/img/carte.jpg" alt="Carte" />
+          <img src="../assets/img/reun.jpg" alt="Chicago" width="1100" height="500" />
+          <div class="carousel-caption">
+            <h3>Chicago</h3>
+            <p>Thank you, Chicago!</p>
+          </div>
         </div>
         <div class="carousel-item">
-          <img src="../assets/img/cal.jpg" alt="Calendrier" />
+          <img src="../assets/img/carte.jpg" alt="New York" width="1100" height="500" />
+          <div class="carousel-caption">
+            <h3>New York</h3>
+            <p>We love the Big Apple!</p>
+          </div>
         </div>
       </div>
-
-      <!-- Left and right controls -->
       <a class="carousel-control-prev" href="#demo" data-slide="prev">
         <span class="carousel-control-prev-icon"></span>
       </a>
@@ -40,3 +47,9 @@ export default {
   components: {}
 };
 </script>
+<style>
+.carousel-inner img {
+  width: 100%;
+  height: 100%;
+}
+</style>
