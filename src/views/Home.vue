8f856ee93bed4f08aadc778e67d699d8ab680548
Thomas <esomly57@gmail.com>
2020-06-27 14:26:50 +0200

    setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <div id="demo" width="100%" height="10%" class="carousel slide" data-ride="carousel">
+    <div id="demo" width="100%" height="100%" class="carousel slide" data-ride="carousel">
       <ul class="carousel-indicators">
         <li data-target="#demo" data-slide-to="0" class="active"></li>
         <li data-target="#demo" data-slide-to="1"></li>
@@ -8,21 +8,21 @@
       </ul>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="../assets/img/cal.jpg" alt="Los Angeles" width="100%" height="50%" />
+          <img src="../assets/img/cal.jpg" alt="Los Angeles" width="100%" height="100%" />
           <div class="carousel-caption">
             <h3>Indiquez une date !</h3>
             <p>Grâce à l'outil reunionou.app</p>
           </div>
         </div>
         <div class="carousel-item">
-          <img src="../assets/img/reun.jpg" alt="Chicago" width="100%" height="50%" />
+          <img src="../assets/img/reun.jpg" alt="Chicago" width="100%" height="100%" />
           <div class="carousel-caption">
             <h3>Organiser des réunions simplement !</h3>
             <p>Grâce à l'outil reunionou.app</p>
           </div>
         </div>
         <div class="carousel-item">
-          <img src="../assets/img/carte.jpg" alt="New York" width="100%" height="50%" />
+          <img src="../assets/img/carte.jpg" alt="New York" width="100%" height="100%" />
           <div class="carousel-caption">
             <h3>Indiquez une adresse</h3>
             <p>Grâce à l'outil reunionou.app</p>
@@ -52,7 +52,7 @@ export default {
   filter: blur(5px);
   -webkit-filter: blur(5px);
   width: 100%;
-  height: 50%;
+  height: 100%;
 }
 .carousel-caption {
   transform: translateY(-50%);
