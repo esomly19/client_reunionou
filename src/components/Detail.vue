4df66eaef8aa24770b1c635a79db2e1ec1d2fb23
Thomas <esomly57@gmail.com>
2020-06-28 22:04:02 +0200

    css

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,74 +1,77 @@
 <template>
   <div class="eventlist">
-    <div class="row">
-      <div class="col-8 ga">
-        <l-map
-          style="height: 400px;  width:100%"
-          :zoom="zoom"
-          :center="center"
-          :markerZoomAnimation="true"
-          attribution="ok"
-        >
-          <l-tile-layer :url="url"></l-tile-layer>
-          <l-marker :lat-lng="positionCentre" :draggable="false" :icon="icon"></l-marker>
-        </l-map>
-        <button
-          type="button"
-          class="btn btn-primary"
-          v-clipboard:copy="message"
-          v-clipboard:success="onCopy"
-          v-clipboard:error="onError"
-        >Partager</button>
-        <p>{{events}}</p>
-      </div>
-      <div class="col-4 dr">
-        <ul>
-          <li
-            v-for="comment in comments"
-            :key="comment.nom"
-          >{{ comment.nom }}{{ comment.commentaire }}</li>
-        </ul>
-        <div>
+    <div v-if="load" class="loader"></div>
+    <div v-if="!load">
+      <div class="row">
+        <div class="col-8 ga">
+          <l-map
+            style="height: 400px;  width:100%"
+            :zoom="zoom"
+            :center="center"
+            :markerZoomAnimation="true"
+            attribution="ok"
+          >
+            <l-tile-layer :url="url"></l-tile-layer>
+            <l-marker :lat-lng="positionCentre" :draggable="false" :icon="icon"></l-marker>
+          </l-map>
           <button
             type="button"
             class="btn btn-primary"
-            data-toggle="modal"
-            data-target="#myModal"
-            data-backdrop="true"
-          >Commenter</button>
+            v-clipboard:copy="message"
+            v-clipboard:success="onCopy"
+            v-clipboard:error="onError"
+          >Partager</button>
+          <p>{{events}}</p>
+        </div>
+        <div class="col-4 dr">
+          <ul>
+            <li
+              v-for="comment in comments"
+              :key="comment.nom"
+            >{{ comment.nom }}{{ comment.commentaire }}</li>
+          </ul>
+          <div>
+            <button
+              type="button"
+              class="btn btn-primary"
+              data-toggle="modal"
+              data-target="#myModal"
+              data-backdrop="true"
+            >Commenter</button>
 
-          <div
-            class="modal fade"
-            id="myModal"
-            tabindex="-1"
-            role="dialog"
-            aria-labelledby="exampleModalCenterTitle"
-            aria-hidden="true"
-          >
-            <div class="modal-dialog modal-dialog-centered" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div class="modal-body">
-                  <input
-                    type="text"
-                    id="name"
-                    v-model="name"
-                    name="name"
-                    required
-                    minlength="4"
-                    maxlength="8"
-                    size="10"
-                    placeholder="nom"
-                  />
-                  <textarea id="name" v-model="description" name="name" />
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-success" v-on:click="commenter">Poster</button>
+            <div
+              class="modal fade"
+              id="myModal"
+              tabindex="-1"
+              role="dialog"
+              aria-labelledby="exampleModalCenterTitle"
+              aria-hidden="true"
+            >
+              <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <input
+                      type="text"
+                      id="name"
+                      v-model="name"
+                      name="name"
+                      required
+                      minlength="4"
+                      maxlength="8"
+                      size="10"
+                      placeholder="nom"
+                    />
+                    <textarea id="name" v-model="description" name="name" />
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-success" v-on:click="commenter">Poster</button>
+                  </div>
                 </div>
               </div>
             </div>
@@ -109,6 +112,7 @@ export default {
       name: "",
       description: "",
       ville: null,
+      load: false,
       meteod: null,
       zoom: 10,
       url:
@@ -153,6 +157,7 @@ export default {
         });
     },
     recupevents() {
+      this.load = true;
       axios
         .get(
           "https://warm-badlands-86536.herokuapp.com/event/" +
@@ -166,6 +171,7 @@ export default {
           this.positionCentre.lng = res.data.y;
           this.ville = res.data.ville;
           this.events = res.data;
+          this.load = false;
           //this.meteo();
         })
         .catch(err => {
@@ -229,6 +235,34 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.loader {
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #3498db;
+  width: 120px;
+  height: 120px;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+}
+
+/* Safari */
+@-webkit-keyframes spin {
+  0% {
+    -webkit-transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 .modal.show {
   z-index: 100000 !important;
 }
@@ -238,7 +272,6 @@ export default {
 }
 
 .dr {
-  border: 2px solid black;
   height: 100%;
 }
 
