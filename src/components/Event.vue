a3ca0c99a3ebe38c0f21a24e8d414b8c351a11c8
Thomas <esomly57@gmail.com>
2020-06-29 14:34:56 +0200

    correctio,n

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -54,27 +54,26 @@
           <l-tile-layer :url="url"></l-tile-layer>
           <l-marker :lat-lng.sync="positionCentre" :draggable="true" :icon="icon" id="rr"></l-marker>
         </l-map>
-        <div class="col-5">
-          <input type="text" placeholder="Entrez votre adresse" v-model="adresse" />
 
-          <button class="button" v-on:click="getLocation">Marquer !</button>
-          <div v-if="wait">...</div>
-          <section v-if="erreur">
-            <p>Désolé, mais nous n'avons trouvé aucun résultat pour {{ adresse }}.</p>
-          </section>
+        <input type="text" placeholder="Entrez votre adresse" v-model="adresse" />
 
-          <section v-if="resultats">
-            <div v-if="loading">Chargement...</div>
-            <div v-else v-for="poi in resultats" v-bind:key="poi.address">
-              <h2>Résultats pour {{ adresse }}</h2>
-              <input type="number" :value="positionCentre.lat" />
-              <input type="number" :value="positionCentre.lng" />
-              <div v-if="poi.address.postcode">
-                <h3>{{ poi.address.postcode }} {{ poi.address.city }} {{ poi.address.suburb }}</h3>
-              </div>
+        <button class="button" v-on:click="getLocation">Marquer !</button>
+        <div v-if="wait">...</div>
+        <section v-if="erreur">
+          <p>Désolé, mais nous n'avons trouvé aucun résultat pour {{ adresse }}.</p>
+        </section>
+
+        <section v-if="resultats">
+          <div v-if="loading">Chargement...</div>
+          <div v-else v-for="poi in resultats" v-bind:key="poi.address">
+            <h2>Résultats pour {{ adresse }}</h2>
+            <input type="number" :value="positionCentre.lat" />
+            <input type="number" :value="positionCentre.lng" />
+            <div v-if="poi.address.postcode">
+              <h3>{{ poi.address.postcode }} {{ poi.address.city }} {{ poi.address.suburb }}</h3>
             </div>
-          </section>
-        </div>
+          </div>
+        </section>
       </div>
     </div>
     <p>
