d0da1c4c74a72a96fceba77ade88a8ce18a79004
Thomas <esomly57@gmail.com>
2020-07-02 13:45:56 +0200

    setup

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -419,4 +419,32 @@ html {
   height: 70%;
   overflow-y: hidden;
 }
+
+/* Style all font awesome icons */
+.fa {
+  padding: 20px;
+  font-size: 30px;
+  width: 50px;
+  text-align: center;
+  text-decoration: none;
+}
+
+/* Add a hover effect if you want */
+.fa:hover {
+  opacity: 0.7;
+}
+
+/* Set a specific color for each brand */
+
+/* Facebook */
+.fa-facebook {
+  background: #3b5998;
+  color: white;
+}
+
+/* Twitter */
+.fa-twitter {
+  background: #55acee;
+  color: white;
+}
 </style>
